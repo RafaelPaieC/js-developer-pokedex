9944adff6870855809364a8bd76c9903725dff73
rafaelpaie <rafaelpaie.c@gmail.com>
2023-09-18 20:45:05 -0300

    Criando mecanismo para limitar na primeira geração de Pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
-const limit = 5;
+
+const maxRecords = 151
+const limit = 10;
 let offset = 0;
+
+
 
 function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
@@ -30,5 +34,14 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener("click", () => {
   offset += limit
-  loadPokemonItens(offset, limit);
+  const qtdRecordWithNextPage = offset + limit
+
+  if (qtdRecordWithNextPage >= maxRecords) {
+    const newLimit = maxRecords - offset
+    loadPokemonItens(offset, newLimit)
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+  } else {
+      loadPokemonItens(offset, limit)
+  }
 });
